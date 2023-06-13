4587141ba508cacd8907ed351d439748e5d034d6
Ivaylo Tsochev <ivo.tsochev@live.com>
2023-06-13 23:56:09 +0300

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="">
+      {/* Header */}
+      <Header />
+
+      {/* Board */}
       <h1>Helloooo</h1>
     </main>
   )
